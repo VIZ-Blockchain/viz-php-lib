85dd8a02f6595f4c20d59a74d1d6fd5146c60c1e
Vladimir Babin <vovababin@gmail.com>
2020-10-06 18:27:05 +0500

    Add test for signature verification

--- tests/TestKeys.php
+++ tests/TestKeys.php
@@ -17,4 +17,14 @@ final class TestKeys extends TestCase
             'VIZ5MY1yEaHaJ9i6W2UWFH57Kscr7aRgz9HksQy9QQWMhUvUJWXn4'
         );
     }
+
+    public function testVerifySignature(): void
+    {
+        $private_key = new VIZ\Key('b9f3c242e5872ac828cf2ef411f4c7b2a710bd9643544d735cc115ee939b3aae');
+        $data = 'Hello VIZ.World!';
+        $signature = $private_key->sign($data);
+        $public_key = $private_key->get_public_key();
+        $this->assertTrue($public_key->verify($data, $signature));
+        $this->assertFalse($public_key->verify('Bye VIZ.World!', $signature));
+    }
 }
